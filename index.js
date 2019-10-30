0e4b070e1f186b2b28f4731af232ba8087b5377f
Alyona <itsbronza@yahoo.com>
2019-10-30 06:27:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
+'Hello, ' + specialGuest + '!'
+
 var greetSpecialGuest = "" + specialGuest + "!";
 
 var topic = "space";
 
 var conversation = `${topic}`;
 
-'Hello, ' + specialGuest + '!'
+
